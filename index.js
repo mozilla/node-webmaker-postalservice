ea6a07a8a5b5855e8c72581ca6ecc42aa6a13d59
Kate Hudson <k88hudson@gmail.com>
2014-05-12 18:15:22 -0400

    Bug 1009204 - Postal service should only render emails

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-var AWS = require("aws-sdk"),
-    nunjucks = require("nunjucks"),
+var nunjucks = require("nunjucks"),
     nunjucksEnv = new nunjucks.Environment(
       new nunjucks.FileSystemLoader(__dirname + "/templates/"),
       { autoescape: true }
     ),
-    premailer = require('premailer-api'),
-    i18n = require('webmaker-i18n'),
-    path = require('path');
+    i18n = require("webmaker-i18n"),
+    path = require("path");
 
 // Setup locales with i18n
 i18n.middleware({
-  supported_languages: ['*'],
+  supported_languages: ["*"],
   default_lang: "en-US",
   translation_directory: path.resolve(__dirname, "locale")
 });
@@ -24,183 +22,26 @@ function isLanguageSupport(locale) {
   return i18n.getSupportLanguages().indexOf(locale) !== -1;
 };
 
-module.exports = function(options) {
-  if (!options.key) {
-    throw 'aws "key" required';
-  }
-  if (!options.secret) {
-    throw 'aws "secret" required';
-  }
+module.exports = function () {
 
-  var ses = new AWS.SES({
-    accessKeyId: options.key,
-    secretAccessKey: options.secret
+  var templates = {};
+
+  // Add all templates from templates folder to nunjucks environment
+  fs.readdirSync(__dirname + "/templates").forEach(function (file) {
+    var name = path.basename(file, ".js");
+    templates[name] = nunjucksEnv.getTemplate(file)
   });
 
-  var templates = {
-    createEventEmail: nunjucksEnv.getTemplate("create_event.html"),
-    mofoStaffEmail: nunjucksEnv.getTemplate("mofo_staff_new_event.html"),
-    welcomeEmail: nunjucksEnv.getTemplate("welcome.html"),
-    badgeAwarded: nunjucksEnv.getTemplate("badge_awarded.html")
+  // Render the email
+  return function renderEmail(templateName, data, locale) {
+    var template = templates[templateName];
+    locale = isLanguageSupport(locale) ? locale : "en-US";
+
+    data = data || {};
+    data.locale = locale;
+    data.gettext = i18n.getStrings(locale);
+
+    return template.render(data);
   };
 
-  return {
-    sendCreateEventEmail: function(options, callback) {
-      options.locale = isLanguageSupport(options.locale) ? options.locale : "en-US";
-      var html = templates.createEventEmail.render({
-        fullName: options.fullName,
-        gettext: i18n.getStrings(options.locale),
-        locale: options.locale
-      });
-
-      premailer.prepare({
-        html: html
-      }, function(err, email) {
-        if (err) {
-          return callback(err);
-        }
-
-        ses.sendEmail({
-          Source: "events@webmaker.org",
-          Destination: {
-            ToAddresses: [options.to],
-          },
-          Message: {
-            Subject: {
-              Data: i18n.gettext("Next steps for your event", options.locale),
-              Charset: "utf8"
-            },
-            Body: {
-              Text: {
-                Data: email.text,
-                Charset: "utf8"
-              },
-              Html: {
-                Data: email.html,
-                Charset: "utf8"
-              }
-            }
-          }
-        }, callback);
-      });
-    },
-    sendBadgeAwardedEmail: function(options, callback) {
-      options.locale = isLanguageSupport(options.locale) ? options.locale : "en-US";
-      var html = templates.badgeAwarded.render({
-        email: options.email,
-        badge: options.badge,
-        comment: options.comment,
-        gettext: i18n.getStrings(options.locale),
-        locale: options.locale
-      });
-
-      premailer.prepare({
-        html: html
-      }, function(err, email) {
-        if (err) {
-          return callback(err);
-        }
-
-        ses.sendEmail({
-          Source: "help@webmaker.org",
-          Destination: {
-            ToAddresses: [options.to]
-          },
-          Message: {
-            Subject: {
-              Data: i18n.gettext("badgesTitle", options.locale),
-              Charset: "utf8"
-            },
-            Body: {
-              Text: {
-                Data: email.text,
-                Charset: "utf8"
-              },
-              Html: {
-                Data: email.html,
-                Charset: "utf8"
-              }
-            }
-          }
-        }, callback);
-      });
-    },
-    sendWelcomeEmail: function(options, callback) {
-      options.locale = isLanguageSupport(options.locale) ? options.locale : "en-US";
-      var html = templates.welcomeEmail.render({
-        fullName: options.fullName,
-        gettext: i18n.getStrings(options.locale),
-        locale: options.locale
-      });
-
-      premailer.prepare({
-        html: html
-      }, function(err, email) {
-        if (err) {
-          return callback(err);
-        }
-
-        ses.sendEmail({
-          Source: "help@webmaker.org",
-          Destination: {
-            ToAddresses: [options.to]
-          },
-          Message: {
-            Subject: {
-              Data: i18n.gettext("emailTitle", options.locale),
-              Charset: "utf8"
-            },
-            Body: {
-              Text: {
-                Data: email.text,
-                Charset: "utf8"
-              },
-              Html: {
-                Data: email.html,
-                Charset: "utf8"
-              }
-            }
-          }
-        }, callback);
-      });
-    },
-    sendMofoStaffEmail: function(options, callback) {
-      var html = templates.mofoStaffEmail.render({
-        email: options.email,
-        username: options.username,
-        eventId: options.eventId
-      });
-
-      premailer.prepare({
-        html: html
-      }, function(err, email) {
-        if (err) {
-          return callback(err);
-        }
-
-        ses.sendEmail({
-          Source: "help@webmaker.org",
-          Destination: {
-            ToAddresses: [options.to]
-          },
-          Message: {
-            Subject: {
-              Data: "A new event was created",
-              Charset: "utf8"
-            },
-            Body: {
-              Text: {
-                Data: email.text,
-                Charset: "utf8"
-              },
-              Html: {
-                Data: email.html,
-                Charset: "utf8"
-              }
-            }
-          }
-        }, callback);
-      });
-    }
-  };
 };
